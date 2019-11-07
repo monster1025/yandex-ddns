bb2280e64600ab5575937af36507ab769fe8e8ca
Your Name <you@example.com>
2019-11-07 15:36:36 +0300

    not cating the config

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ do
   echo "subdomain=$SUBDOMAIN" >> /yandex-ddns.toml
   echo "ttl=$TTL" >> /yandex-ddns.toml
   chmod 600 /yandex-ddns.toml
-  cat /yandex-ddns.toml
+  #cat /yandex-ddns.toml
   ./app
   sleep 5m
 done
